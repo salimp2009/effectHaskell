b766fd3ee726105863a68ebfb812f06dafa301e3
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-22 00:04:48 +0300

    added more examples for destructuring values with PatternMatching also created a Haddock using stack haddock

--- src/DestructureValues.hs
+++ src/DestructureValues.hs
@@ -13,5 +13,22 @@ matchNum :: (Eq a, Num a, Show a) => a -> String
 matchNum 0 = "zero"
 matchNum n = show n
 
+matchList :: (Eq a, Num a, Show a) => [a] -> String
+matchList [1,2,3] = "one, two, there => here we go"
+matchList list = show list
 
+matchBool::Bool -> String
+matchBool True = "this is so True :)"
+matchBool bool = show bool
+
+matchTuple :: (String, String) -> String
+matchTuple ("hello", "world") = "Hello Demir => we never forget you bro"
+matchTuple ("hello", name) = "hello yow " <> name
+matchTuple (greeting, "Dido") =  greeting <> "didoki min :)" 
+matchTuple n =  show n
+
+
+matchBool'::Bool -> String
+matchBool' True = "this is so True :)"
+matchBool' False = "so wrong => Falseee:)"
 
