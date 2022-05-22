006a53771156b01fe5b5afc7e34414e20474c961
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-23 01:51:13 +0300

    added a fibonacci from the book example

--- src/DestructureValues.hs
+++ src/DestructureValues.hs
@@ -24,11 +24,29 @@ matchBool bool = show bool
 matchTuple :: (String, String) -> String
 matchTuple ("hello", "world") = "Hello Demir => we never forget you bro"
 matchTuple ("hello", name) = "hello yow " <> name
-matchTuple (greeting, "Dido") =  greeting <> "didoki min :)" 
+matchTuple (greeting, "Dido") =  greeting <> "didoki min :)"
 matchTuple n =  show n
-
 
 matchBool'::Bool -> String
 matchBool' True = "this is so True :)"
 matchBool' False = "so wrong => Falseee:)"
+
+addValues :: Num a => [a] -> a
+addValues = foldr (+) 0
+
+addValues'::(Foldable t, Num a) => t a -> a
+addValues' = sum
+
+fibs' :: Integer -> Integer
+fibs' n 
+    | n == 0 = 0
+    | n <= 2 = 1
+    | otherwise =  fibs' (n-1) + fibs'(n-2)
+
+-- | this infinite list use with take
+-- e.g take 5 fibs = [1,1,2,3,5]
+fibs :: [Integer]
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+
 
