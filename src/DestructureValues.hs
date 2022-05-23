da823358ca8f95eb2b7a94a482f5d7f4040e7228
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-24 01:24:01 +0300

    added more examples using pattern matching inside the function . Prev examples use pattern matching at function level; this time inside the function using parameters also added GHC flag for incomplete patterns

--- src/DestructureValues.hs
+++ src/DestructureValues.hs
@@ -55,10 +55,32 @@ printFibonacci n =
         fibonList = show $ take n fibs
     in "finonacci number is : " <> fibonNum <> " and the list is : " <> fibonList
 
+-- | p@(a,b) means p is a tuple as single variable; 
+-- you can use p or elements of tuple a and b
 modifyPair :: (String, String) -> String
 modifyPair p@(a, b)
         | a == "Hello" = "this is a salute : " <> a
         | b == "George" || b== "Salim" = "this is a message to " <> b
         | otherwise = "I don't know what " <> show p <> "is!! "
 
+favoriteFood :: String -> String
+favoriteFood person = 
+    case person of
+        "Salim" -> "ice cream"
+        "Semos" -> person <> " loves baklava"
+        "Demir" -> "Nutella"
+        "Didem" -> "chocolate" 
+        name    -> "no idea " <> name <> " is going for"
 
+handleNums :: (Integral a, Show a) => [a] -> String
+handleNums lst = 
+    case lst of
+        [] -> "empty list"
+        [x] | x == 0 -> "list of [0]"
+            | x == 1 -> "lonely list [1]" 
+            | even x -> "singleton list with even number " <> show [x]
+            | otherwise ->  "list contains " <> show x
+        _lst -> "list of multiple elems " <> show lst
+
+-- partialFunction :: (Eq a, Num a) => a -> String
+-- partialFunction 0 = "only working for Mr. ZEro :)"
--- src/PatternMatch.hs
+++ src/PatternMatch.hs
@@ -12,3 +12,13 @@ myThrd (_, _, x) = x
 -- | usage: e.g: applytoThruple [(1,2,3)] -> [1,2,3]
 applytoThruple :: [(b, b, b)] -> [b]
 applytoThruple = (<*>) [myFst, mySnd, myThrd] 
+
+-- | usage: e.g: applytoThruple' (1,2,3) -> [1,2,3]
+applytoThruple' :: (b, b, b) -> [b]
+applytoThruple' as = map ($ as) [myFst, mySnd, myThrd]
+
+-- | _tail is used to communicate _tail will not be used
+printHead ::  Show a =>  [a] -> String
+printHead [] = "empty!"
+printHead lst@(hd: _tail) = 
+            "the head of " <> show lst <> " is " <> show hd
