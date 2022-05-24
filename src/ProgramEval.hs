75cb1933faa483d38a580de9425d242e88af74e4
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-25 01:35:41 +0300

    started example for corecursion which is infinite cycles that starts and never ends and called lazly

--- src/ProgramEval.hs
+++ src/ProgramEval.hs
@@ -3,3 +3,9 @@ module ProgramEval where
 numbersStartingAt :: Num t => t -> [t]
 numbersStartingAt n =
             n: numbersStartingAt (n+1)
+
+radsToDegrees::Float -> Int
+radsToDegrees radians =
+    let degrees = cycle [0..359]
+        converted = truncate $ (radians * 360) / (2 * pi)
+    in degrees !! converted
