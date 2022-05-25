33fa3871a45925150425de0e5c25f28703c82b86
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-25 15:29:09 +0300

    added custom cycle implementations

--- src/ProgramEval.hs
+++ src/ProgramEval.hs
@@ -9,3 +9,19 @@ radsToDegrees radians =
     let degrees = cycle [0..359]
         converted = truncate $ (radians * 360) / (2 * pi)
     in degrees !! converted
+
+-- | Manual/Naive way of creating lazy cycle
+-- usage: epicCycle [0..4] $  !! 10  -> 0 ; 
+-- e.g: similar to modulo operation 10 `mod` 5
+epicCycle :: [a] -> [a]
+epicCycle inputList = 
+            cycleHelper inputList 
+            where
+                cycleHelper [] = epicCycle inputList
+                cycleHelper (x:xs) = x : cycleHelper xs
+
+-- | Similar to above creating cycle lazily
+moreEpicCycle :: Semigroup t => t -> t
+moreEpicCycle inputList =
+     inputList <> moreEpicCycle inputList
+
