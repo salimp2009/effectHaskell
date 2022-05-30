30a8173b22976c2d3b22af092e875ba4988ff06f
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-31 01:30:49 +0300

    added type annotation for function example newaddThreeeNum by splitting into 3 functions using let and each function has their own type annotation then return the top level function with the input variable applied

--- src/BasicTypes.hs
+++ src/BasicTypes.hs
@@ -9,3 +9,21 @@ calculateTotalCost basePrice =
         priceWithServiceFee = basePrice  + 20
         customaryTip = 6 :: Int
     in priceWithServiceFee + customaryTip
+
+newaddOne :: Int -> Int
+newaddOne = (+ 1)
+
+newaddThreeNums :: Int -> (Int -> (Int -> Int))
+newaddThreeNums x y z = 
+    let 
+        f:: Int -> (Int -> (Int -> Int))
+        f a =
+            let 
+                g :: Int -> (Int -> Int)
+                g b =
+                  let 
+                    h :: Int -> Int
+                    h c = a + b + c 
+                  in h
+            in g 
+    in f x y z
