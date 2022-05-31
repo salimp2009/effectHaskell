aab17d8d74429a7ce2f92982fa5ed2dfc068507e
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-05-31 15:21:35 +0300

    added example for Scope type variable application for let and where binding type annotations where the generic types at the very top level wont match the scoped level unless we use forall a b. and language extesions also show example how to express functions as arguments to other functions need to put them in paranthesis otherwise they will be deduced as multiple non function arguments

--- src/BasicTypes.hs
+++ src/BasicTypes.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+-- ^ These are used for the example working / working Scoped Types Variable
+
 module BasicTypes where
 
 -- | basic example to show how type annotations can be added
 -- to let and where bindings
 calculateTotalCost :: Int -> Int
-calculateTotalCost basePrice = 
+calculateTotalCost basePrice =
     let
         priceWithServiceFee :: Int
         priceWithServiceFee = basePrice  + 20
@@ -14,16 +18,67 @@ newaddOne :: Int -> Int
 newaddOne = (+ 1)
 
 newaddThreeNums :: Int -> (Int -> (Int -> Int))
-newaddThreeNums x y z = 
-    let 
+newaddThreeNums x y z =
+    let
         f:: Int -> (Int -> (Int -> Int))
         f a =
-            let 
+            let
                 g :: Int -> (Int -> Int)
                 g b =
-                  let 
+                  let
                     h :: Int -> Int
-                    h c = a + b + c 
+                    h c = a + b + c
                   in h
-            in g 
+            in g
     in f x y z
+
+-- | example from book Thinking types
+-- only works when forall is applied and also have to use 
+-- -XScopedTypeVariables extension
+-- if no type annotation is supplied for apply it works; compiler works it
+working :: forall p t. (p -> t) -> p -> t
+working f a = apply
+    where
+        apply :: t
+        apply = f a
+
+-- broken :: p1 -> p2 -> t
+-- broken f a = apply
+--     where
+--         apply :: t
+--         apply = f a 
+
+working''::(a -> b) -> a ->b
+working'' f a = apply
+    where apply = f a
+
+-- | this work all type variable are explicitly Int
+-- if this is generic a ->b ->b -> b then
+-- we had to use forall a b . and for the scope type variable
+-- otherwise 
+-- worksToo :: (Int -> Int) -> Int -> Int
+worksToo :: forall a b. (a -> b) -> a -> b
+worksToo f a =
+     let
+        -- apply:: (Int -> Int) -> Int -> Int
+        apply:: (a -> b) -> a -> b
+        apply f' =
+             let
+                 --applymore :: Int -> Int
+                 applymore ::  a -> b
+                 applymore a' = f' a'
+             in applymore
+    in apply f a  
+    
+ -- | use case;  incrementAndShow 2 show -> "3" 
+incrementAndShow :: Int -> (Int -> String) -> String
+incrementAndShow n formatter = formatter (n+1)
+
+-- | use case : 
+-- incrementAndShow' 2 (&) -> "3"
+-- ^ need to import Data.Function ((&)) to be able to use it
+-- | use case ; 
+-- incrementAndShow' 2 incrementAndShow -> "4"
+incrementAndShow' :: Int -> (Int -> (Int ->String)->String) ->String
+incrementAndShow' n f = f  (n+1) show 
+        
