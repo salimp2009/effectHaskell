d14826b2c28beb4c742f4b0b6bdac5e762fef76c
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-06-04 01:02:30 +0300

    added some exercises mySwap, myConcat and myConcat' which is using myBuild GHC build function inline foldr

--- src/TypeHoleUses.hs
+++ src/TypeHoleUses.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module TypeHoleUses where
 
 exampleNums :: [Int]
@@ -6,7 +9,7 @@ exampleNums = [1..10]
 -- | this wont compile and error due to type hole after quantity _
 -- the error messages will tell us what type needed to work; a tech that can be used
 -- if we cant figure out the type 
-    
+
 -- getFiveNUms :: [Int]
 -- getFiveNUms = let quantity = 5 in take quantity _
 
@@ -17,26 +20,43 @@ mergeFirstTwo :: (a,b,c) ->(a->b->d) -> (d,c)
 mergeFirstTwo (a, b, c) f = (f a b , c)
 
 showFields :: String
-showFields = 
+showFields =
     let (a,b) = combinePermutations . permuteThruple $ ("hello", "world", 10)
     in unlines [fst a , fst b]
     where
         joinFields a b = show a <> " - " <> b
-        combinePermutations (a,b,c,d,e,f) = 
-            ( mergeFirstTwo a joinFields, 
+        combinePermutations (a,b,c,d,e,f) =
+            ( mergeFirstTwo a joinFields,
               mergeFirstTwo e joinFields
             )
 
 showStringPair::(String, String) -> String
 showStringPair (a,b) = "fst: " <> a <> ", snd: " <> b
 
-doubleField :: a -> (a, a)
+doubleField ::a -> (a, a)
 doubleField a = (a,a)
 
 showValues::String
-showValues = unlines $ map (showStringPair . doubleField . show) [1..10]
+showValues = unlines $ map (showStringPair . doubleField . show ) [1..10]
 
 -- | Using type hole to determine the type; original doubleField accepts
 -- any type since it is Num, Enum it will not work showStringPair
 -- when used with the type hole compiler show exact problem and how to fix it
 -- showValues = unlines $ map (showStringPair . _doubleField ) [1..10]
+
+mySwap :: (a , b) -> (b , a)
+mySwap (a,b) = (b,a)
+
+myConcat :: [[a]] -> [a]
+myConcat = foldr (<>) []
+
+-- | myBuild is build in GHC implementation
+myConcat' :: forall a t1 t2. (Foldable t1, Foldable t2) => t1 (t2 a) -> [a]
+myConcat'  xs = myBuild (\c n -> foldr (\x y -> foldr c y x) n xs)
+    where
+      myBuild::((a -> [a] -> [a]) -> [a1] -> t) -> t
+      myBuild g = g (:) []
+      {-# INLINE myBuild #-}
+{-# INLINE myConcat' #-}
+
+
