a21b4ed3ab33475708975ac703969466de1ca408
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-06-07 15:09:45 +0300

    added some example how to use Proxy by creating a type class and define a function that is dependant on the type passed to Proxy also create a constraint to use thephantom type of a type to use with the Proxy

---
+++ src/SumTypes.hs
@@ -0,0 +1,4 @@
+module SumTypes where
+
+-- | using Sum types as Enums
+data Direction = North | South | East | West
--- src/TermsTypesKinds.hs
+++ src/TermsTypesKinds.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module TermsTypesKinds where
 
--- example for temperature and using phantom types
+import Data.Proxy (Proxy(..))
+
+-- example for temperature and using phantom types; 
 -- unit is a phantom type but it has a flaw cause
 newtype Temp unit = Temp Double
-    deriving (Num, Fractional, Show)
+    deriving (Num, Fractional)
 
 -- types to use as unit in Temp
 -- no value
@@ -26,3 +30,42 @@ fahrenheitToCelcius (Temp f) = Temp ((f-32) * 5 / 9)
 -- unit == Bool but Temp value is still double
 nonsense :: Temp Bool
 nonsense = 0
+
+-- | Example to use Proxy which uses a phantom type t but does not use the value
+-- data Proxy t = Proxy
+
+-- | Proxy is used to create different functionality for unitname 
+-- depending on the type
+class UnitName u where
+    unitName :: Proxy u -> String
+
+-- | type annotation for unitName is not neccessary since it is inferred
+-- if used then have use the {-# LANGUAGE InstanceSigs #-} extension
+instance UnitName C where
+    unitName :: Proxy C -> String   
+    unitName _ = "C"
+
+instance UnitName F where
+    unitName :: Proxy F -> String   
+    unitName _  = "F"
+
+-- | we refer to unit inside Temp unit
+-- therefore we need extension {-# LANGUAGE ScopedTypeVariables #-}
+instance UnitName unit => UnitName (Temp unit) where
+    -- unitName :: forall u. UnitName u => Proxy u -> String
+    -- unitName :: Proxy (Temp unit) -> String
+    unitName _ = unitName (Proxy::Proxy unit)
+
+instance UnitName unit => Show (Temp unit) where
+    show (Temp t)= show t <> "°" <> unitName (Proxy::Proxy unit)
+
+data K
+
+instance UnitName K where
+    unitName :: Proxy K -> String
+    unitName _ = "K"
+
+unit :: forall u. UnitName u => Temp u -> String
+unit _ = unitName (Proxy::Proxy u)
+
+
