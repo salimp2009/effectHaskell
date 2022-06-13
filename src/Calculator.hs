f6ec21ebfa39516de37009d770de8d5ea3c5bc8c
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-06-13 15:36:09 +0300

    started added our expression and eval functions to make our calculator then we need to parse to turn written Strings into expressions and eval them; incomplete

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -1,6 +1,6 @@
 -- {-# LANGUAGE RankNTypes #-}
 -- {-# LANGUAGE TypeApplications #-}
--- testing git shortcut
+-- {-# LANGUAGE KindSignatures #-}
 
 module Calculator where
 
@@ -12,11 +12,55 @@ data Expr = Lit Int
           | Div Expr Expr
           deriving (Show)
 
+-- | First version; revised to avoid eval repitition 
 -- eval :: forall a. (Num a, Integral a) => Expr -> a
-eval expr =
+-- eval :: Expr -> Int
+-- eval expr =
+--     case expr of
+--         Lit num -> num 
+--         Add x y -> eval x + eval y
+--         Sub x y -> eval x - eval y
+--         Mul x y -> eval x * eval y
+--         Div x y -> eval x `div` eval y
+
+-- | Use;cases ;
+-- eval $ Add  (Lit 1) (Sub (Lit 5) (Add (Lit 10 `Div`Lit 2) (Lit 3)))
+-- -> -2 
+-- eval $ Add (Lit 1) (Lit 2) -> 3
+eval :: Expr -> Int
+eval expr = 
     case expr of
-        Lit num -> num 
-        Add x y -> eval x + eval y
-        Sub x y -> eval x - eval y
-        Mul x y -> eval x * eval y
-        Div x y -> eval x `div` eval y
+        Lit num -> num
+        Add x y -> eval' (+) x y
+        Sub x y -> eval' (-) x y
+        Mul x y -> eval' (*) x y
+        Div x y -> eval' div x y
+        where
+            eval' operator arg1 arg2 =
+                operator (eval arg1)  (eval arg2)
+
+  -- ^ value constructor are normal function and can be used as infix function
+  
+parse :: String -> Either String Expr
+parse str =
+   case parse' (words str) of
+      Left err        -> Left err
+      Right (e, [])   -> Right e
+      Right (_, rest)  -> Left $ "Found extra tokens: " <> unwords rest
+
+parse' :: [String] -> Either String (Expr, [String])
+parse' = undefined
+
+
+
+
+    
+
+                
+
+
+
+
+
+
+
