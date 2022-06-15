2f634254303206e8454186b9af7e649d7ca1a0c9
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-06-16 01:55:45 +0300

    added more examples for parser using combinedParsers

--- src/ParserExamples.hs
+++ src/ParserExamples.hs
@@ -9,29 +9,29 @@ import Data.List (splitAt)
 -- data StringParser = StringParser { runStringParser :: String -> (String , String)}
 
 -- | revised to newtype as recommended by HLS
-newtype StringParser = StringParser { runStringParser :: String -> (String , String)} 
-                                 
+newtype StringParser = StringParser { runStringParser :: String -> (String , String)}
+
 takeCharacters:: Int -> [a] -> ([a], [a])
 takeCharacters = splitAt
 
 takeCharacters':: Int -> (String -> (String, String))
 takeCharacters' numCharacters = stringParser
-        where 
+        where
             stringParser :: String -> (String , String)
             stringParser = splitAt numCharacters
 
 takeCharacters'':: Int -> (String -> (String, String))
 takeCharacters'' numCharacters = stringParser
-        where 
+        where
             stringParser :: String -> (String , String)
-            stringParser = \inputString -> 
+            stringParser = \inputString ->
                 splitAt numCharacters inputString
 
 takeCharacters''' :: Int -> StringParser
 takeCharacters''' numCharacters = StringParser stringParser
         where
             stringParser :: String -> (String , String)
-            stringParser = \inputString -> 
+            stringParser = \inputString ->
                 splitAt numCharacters inputString
 
 -- | this function takes an Int and gives you a type StringParser 
@@ -45,15 +45,15 @@ takeCharacters''' numCharacters = StringParser stringParser
 -- newParse2 = takeCharactersR  
 -- >runStringParser (newParse2 4) $ "123dsfsfsd" -> ("123d","sfsfsd")
 takeCharactersR :: Int -> StringParser
-takeCharactersR numCharacters = StringParser $ \inputString -> 
+takeCharactersR numCharacters = StringParser $ \inputString ->
     splitAt numCharacters inputString
-    
+
 -- | similar but compose functions
 -- just apply the number and String to get the result
 -- use case; 
 -- newStrparser 3 "123dsfsfsd" -> ("123","dsfsfsd")
 newStrparser :: Int -> String -> (String, String)
-newStrparser = runStringParser.takeCharactersR 
+newStrparser = runStringParser.takeCharactersR
 
 -- | seperates the given list at the condition is True
 -- and return a list of tuples
@@ -69,6 +69,42 @@ getNextWord = StringParser $ \someString ->
             (nextWord, "")   -> (nextWord, "")
             (nextWord, rest) -> (nextWord, tail rest)
 
+-- | use case; 
+-- getNextWord' "sdfs dsfsdf sdfsdf sdfsf" -> ("sdfs","dsfsdf sdfsdf sdfsf")
+getNextWord' :: String -> (String, String)
+getNextWord' = runStringParser.StringParser $ \someString ->
+        case break (==' ') someString of
+            (nextWord, "")   -> (nextWord, "")
+            (nextWord, rest) -> (nextWord, tail rest)
 
-    
+combineParsers :: StringParser -> StringParser -> StringParser
+combineParsers fstParser sndParser = StringParser $ \someString ->
+        let (_firstPart, firstResult) = runStringParser fstParser someString
+        in  runStringParser sndParser firstResult
+
+-- | use case;
+-- runStringParser combinedParsers "sadas sdsd sda asda" -> ("sdsd","sda asda")
+combinedParsers :: StringParser
+combinedParsers = combineParsers getNextWord getNextWord
+
+-- | use case;
+-- getResultCombinedParsers "sadas sdsd sda asda" -> ("sdsd","sda asda")
+getResultCombinedParsers :: String -> (String, String)
+getResultCombinedParsers = runStringParser combinedParsers
+
+getNextWordAfterTenLetters :: StringParser
+getNextWordAfterTenLetters =  combineParsers (takeCharactersR 10) getNextWord
+
+tenLettersAfterTheFirstWord :: StringParser
+tenLettersAfterTheFirstWord = combineParsers getNextWord (takeCharactersR 10)
+
+-- | Helper function to use / parse StringParser 
+-- use case;
+-- parseString (takeCharactersR 5) "hello, haskell" -> "hello,"
+parseString :: StringParser -> String -> String
+parseString parser inputString =
+    fst $ runStringParser parser inputString
+
+
+
                
