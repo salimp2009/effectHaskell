8c142e65e177263b876fd2b734fa50952cb46417
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-06-15 15:22:04 +0300

    added new Parserer examples using a record type that has a function inside to be able to use the function we use the field name and to construct the record type we have give a function with the given type signature

--- src/ParserExamples.hs
+++ src/ParserExamples.hs
@@ -10,9 +10,7 @@ import Data.List (splitAt)
 
 -- | revised to newtype as recommended by HLS
 newtype StringParser = StringParser { runStringParser :: String -> (String , String)} 
-
-                    
-                
+                                 
 takeCharacters:: Int -> [a] -> ([a], [a])
 takeCharacters = splitAt
 
@@ -35,8 +33,42 @@ takeCharacters''' numCharacters = StringParser stringParser
             stringParser :: String -> (String , String)
             stringParser = \inputString -> 
                 splitAt numCharacters inputString
-            
+
+-- | this function takes an Int and gives you a type StringParser 
+-- which has a function inside that has a type String -> (String, String)
+-- it is a record type ; to be able to use that function
+-- you have to use runStringParser and give a String so it gives (String, String) 
+-- |use case ;
+-- newParse = takeCharactersR 3
+-- runStringParser newParse  "dfs12345"  -> ("dfs","12345")
+-- runStringParser newParse $ "dfs12345" -> ("dfs","12345")
+-- newParse2 = takeCharactersR  
+-- >runStringParser (newParse2 4) $ "123dsfsfsd" -> ("123d","sfsfsd")
 takeCharactersR :: Int -> StringParser
 takeCharactersR numCharacters = StringParser $ \inputString -> 
-    splitAt numCharacters inputString   
+    splitAt numCharacters inputString
+    
+-- | similar but compose functions
+-- just apply the number and String to get the result
+-- use case; 
+-- newStrparser 3 "123dsfsfsd" -> ("123","dsfsfsd")
+newStrparser :: Int -> String -> (String, String)
+newStrparser = runStringParser.takeCharactersR 
+
+-- | seperates the given list at the condition is True
+-- and return a list of tuples
+-- here we stop at the first space
+-- second list will contain everything after the space there we use tail
+-- use case ;
+-- runStringParser getNextWord "salim kalitos pasos" ->("salim","kalitos pasos")
+-- note if we don use the tail in second list of the tuple then we will have space at front
+-- e.g: -> ("salim"," kalitos pasos")  
+getNextWord :: StringParser
+getNextWord = StringParser $ \someString ->
+        case break (==' ') someString of
+            (nextWord, "")   -> (nextWord, "")
+            (nextWord, rest) -> (nextWord, tail rest)
+
+
+    
                
