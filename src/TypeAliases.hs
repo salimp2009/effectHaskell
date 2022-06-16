7d190790a00bd867d21a22cd991ceab1f73fc9cc
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-06-17 01:36:03 +0300

    started adding how to use type aliases correctly ; to apply a type to a partially applied type

--- src/TypeAliases.hs
+++ src/TypeAliases.hs
@@ -1,13 +1,22 @@
 module TypeAliases where
 
 -- | Example of how not to use Type aliases
+type Meters  = Double
+type Seconds = Double
+type MetersPerSecond = Double
 
-velocity :: Double -> Double -> Double
+velocity :: Meters -> Seconds -> MetersPerSecond
 velocity meters seconds = meters / seconds
 
 gravity :: Double
 gravity = 
     let 
-        meters = 9.8 :: Double
-        seconds = 1.0 :: Double
+        meters = 9.8 :: Meters
+        seconds = 1.0 :: Seconds
     in velocity meters seconds 
+
+-- | Right ways to use type aliases
+-- type alias is used to apply a type to a partially applied type
+-- e.g : String' applies Char to NewList and creates NewList Char
+data NewList a = EmptyNew | NewList a
+type String' = NewList Char
