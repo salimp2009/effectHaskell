5a169b04d56e96fa05bfd57d980409e56a80a75a
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-07-26 00:35:48 +0300

    added an example how to use Cont Monad instance to flatten several nested functions also implemented ContT version which has return as a Monad; (a - m r ) -> m r

--- src/RankNTypeExamples.hs
+++ src/RankNTypeExamples.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeApplications #-}
+--{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module RankNTypeExamples where
 
@@ -67,20 +67,74 @@ runCont f =
     in f callback
 
 newtype Cont a = Cont
-    { unCont :: forall r. (a -> r) -> r }  
+    { unCont :: forall r. (a -> r) -> r }
 
 instance Functor Cont where
-    fmap f (Cont c) = Cont $ \c' -> c (c' . f)  
-    --fmap f (Cont c) = Cont $ \c' -> let r' = c'.f in c r'  
+    fmap f (Cont c) = Cont $ \c' -> c (c' . f)
+    -- fmap f (Cont c) = Cont $ \c' -> let r' = c'.f in c r'  
 
 instance Applicative Cont where
     pure x = Cont $ \c -> c x
-    (Cont f) <*>  (Cont a) = Cont $ \br -> 
-                                f $ \ab -> 
-                                a $ br . ab 
+    (Cont f) <*>  (Cont a) = Cont $ \br ->
+                                f $ \ab ->
+                                a $ br . ab
 
 instance Monad Cont where
     --(>>=)       :: forall a b. m a -> (a -> m b) -> m b                                
     (Cont m) >>= k = Cont $ \c ->
                         m $ \a ->
                             unCont (k a) c
+    -- m >>= k = Cont $ \c ->
+    --     unCont m (\a -> unCont (k a) c)
+
+-- | classic way  JavaScript-style “pyramids of doom.”
+-- Cont Monad instance will be used later to flatten it
+withVersionNumber :: (Double -> r) -> r
+withVersionNumber f = f 1.0
+
+withTimestamp :: (Int -> r) -> r
+withTimestamp f = f 1532083362
+
+withOS :: (String -> r) -> r
+withOS f = f "linux"
+
+-- | use case;
+-- >>>releaseSTring 
+-- "linux-1.01532083362"
+releaseSTring :: String
+releaseSTring =
+    withVersionNumber $ \version ->
+        withTimestamp $ \date    ->
+            withOS $ \os         ->
+                os <> "-" <> show version <> show date
+
+-- | refactor above with Cont (or ContT)
+releaseStringCont :: String
+releaseStringCont = runCont $ unCont $ do
+    version <- Cont withVersionNumber
+    date    <- Cont withTimestamp
+    os      <- Cont withOS
+    pure $ os <> "-" <> show version <> show date
+
+newtype ContT2 r m a = ContT2 { runContT2 :: (a -> m r) -> m r}
+
+-- | "The result of running a CPS computation with 'return' as the
+-- final continuation" GHC implementation (for learning purposes)
+evalContT2 :: Monad m => ContT2 r m r -> m r
+evalContT2 m = runContT2 m return
+{-# INLINE evalContT2 #-}
+
+instance Functor (ContT2 r m) where
+    fmap f m = ContT2 $ \c -> runContT2 m ( c . f)
+
+instance Applicative (ContT2 r m) where
+    --pure x = ContT2 ($ x)
+    pure x = ContT2 $ \c -> c x 
+    -- <*> :: ContT2 r m (a -> b) -> ContT2 r m a -> ContT2 r m b
+    f <*> g = ContT2 $ \c -> runContT2 f $ 
+                       \b -> runContT2 g (c . b)
+
+instance Monad (ContT2 r m) where  
+    return x = ContT2 ($ x)
+    -- >>= :: ContT2 r m a -> (a -> ContT2 r m b) -> ContT2 r m b
+    m >>= f = ContT2 $ \c -> runContT2 m (\a -> runContT2 (f a) c)
