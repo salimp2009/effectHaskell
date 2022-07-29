c2b324d6e5a2d24b64ed75783e7e2a6ff1c67ef9
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-07-29 13:11:32 +0300

    added a wrapper for IORef called STRef s a and created helper function create new STRef , read and write will add modify and safeRunST so we dont leak state but can get the a

--- src/ExistentialsForUnsafeIO.hs
+++ src/ExistentialsForUnsafeIO.hs
@@ -15,3 +15,30 @@ instance Applicative (ST s) where
 
 instance Monad (ST s) where
   ST a >>= f =  seq a $ f a
+
+newtype STRef s a = STRef
+  { unSTRef :: IORef a} 
+  
+-- | creating new STRef;
+-- newIORef    :: a -> IO (IORef a)
+-- unsafePerformIO :: IO (IORef a) -> IORef a
+-- STRef :: IORef a -> STRef s a  
+newSTRef :: a -> ST s (STRef s a)
+newSTRef = pure . STRef . unsafePerformIO . newIORef
+
+-- | read STREf
+-- unSTRef :: STRef s a -> IORef a
+-- readIORef :: IORef a -> IO a
+-- unsafePerformIO :: IO a -> a
+-- pure :: a -> ST s a
+readSTRef:: STRef s a -> ST s a
+readSTRef = pure . unsafePerformIO . readIORef . unSTRef
+
+-- | write STRef
+-- ref :: STRef s a
+-- unSTRef :: STRef s a -> IORef a
+-- writeIORef :: IORef a -> a -> IO ()
+-- unsafePerformIO :: IO () -> () or :: IO a -> a
+-- pure :: () -> ST s ()
+writeSTRef :: STRef s a -> a -> ST s ()
+writeSTRef ref =  pure. unsafePerformIO . writeIORef (unSTRef ref)
