04022321f314ee5543e1097e8013f892c9d28065
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-08-11 13:46:26 +0300

    added more helper functions for OpenSums added a type constraint KnowNat for the type index we are looking and FindElem works on type level which help runtime findElem function to get the type level KnowNat to term level also smart safe consturctor inj for UnsafeOpenSum and prj to check if the stored index matches the list; still in progress

--- src/OpenSums.hs
+++ src/OpenSums.hs
@@ -30,7 +30,8 @@ with existential types."
 import Data.Kind (Type)
 import Data.Proxy
 import Fcf
-import GHC.TypeLits hiding (type (+), type (<=), type (>), type (<), type (<=))
+import qualified GHC.TypeLits as TL
+import GHC.TypeLits hiding (type (+), type (<=), type (>), type (<), type (<=))  
 import Unsafe.Coerce
   
 -- | OpenSum is a container of f t, where t has kind K
@@ -51,14 +52,35 @@ data OpenSum (f::k -> Type)(ts::[k]) where
 -- Eval (FindIndex ((<=) 3) '[1, 2, 3, 1, 2, 3]) :: Maybe Nat
 -- = 'Just 2
 
--- >>>:kind! Eval (FindIndex ((>) 0) '[1,2,3,1,2,3])
--- Eval (FindIndex ((>) 0) '[1,2,3,1,2,3]) :: Maybe Nat
--- = 'Nothing
+-- >>>:kind! Eval (FindIndex ((>) 2) '[1,2,3,1,2,3])
+-- Eval (FindIndex ((>) 2) '[1,2,3,1,2,3]) :: Maybe Nat
+-- = 'Just 0
 type FindElem (key :: k) (ts::[k]) =
   FromMaybe Stuck =<< FindIndex (TyEq key) ts
 
 -- | the returned index thru FindIndex via Just Nat 
--- we can expose this value by using KnownNat  
+-- we can expose this value by using KnownNat 
+--  
+type Member t ts = TL.KnownNat (Eval (FindElem t ts))
 
-type Member t ts = KnownNat (Eval (FindElem t ts))
+-- |the type-level nature of FindElem means we
+-- pay no runtime cost for the computation.
+findElem :: forall t ts. Member t ts => Int
+findElem = fromIntegral . natVal $ (Proxy @(Eval (FindElem t ts)))
 
+-- | smart safe constructor for OpenSum
+-- inj allows injecting a f t into any OpenSum f ts so long
+-- as t is an element somewhere in ts. H
+inj :: forall f t ts. Member t ts => f t -> OpenSum f ts
+inj = UnsafeOpenSum (findElem @t @ts)
+
+-- | runtime check for if ;
+-- the Int type tag inside of OpenSum is the same as
+-- the type we’re trying to extract it as
+-- if same; we use unsafeCoerce to give non-existential t 
+-- if not same; we get Nothing  
+prj :: forall f t ts. Member t ts => OpenSum f ts -> Maybe (f t) 
+prj (UnsafeOpenSum i f) = 
+    if i == findElem @t @ts
+      then Just $ unsafeCoerce f
+      else Nothing
