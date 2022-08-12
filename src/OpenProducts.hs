4989a3643c83df955720466f3c96e9b7324874f4
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-08-13 01:08:01 +0300

    added insert function to OpenProducts and added use cases in progress

--- src/OpenProducts.hs
+++ src/OpenProducts.hs
@@ -39,15 +39,34 @@ data Anyc (f::k -> Type) where
 -- | ts is now keeps track of which type store in Vector Anyc
 -- and it also associates them with names as Symbols so user can provide
 -- names for the contents of the product
-data OpenProduct (f::k -> Type) (ts :: [(k, Symbol)]) where
+data OpenProduct (f::k -> Type) (ts :: [(Symbol, k)]) where
   OpenProduct :: V.Vector (Anyc f) -> OpenProduct f ts
 
 nil :: OpenProduct f '[]  
 nil = OpenProduct V.empty
 
-{- 
+{- |
   "Because all data inside an OpenProduct will be labeled by
   a SYMBOL, we need a way for users to talk about SYMBOLs at
   the term-level."
 -}
+-- | this will allow us to use Key @"myData"  
+-- (needs TypeApplications extension)
 data Key (key :: Symbol) = Key
+
+-- | cons :: a -> Vector a -> Vector a (/O(n)/ Prepend an element.)
+-- use cases;
+-- >>>result = insert ( Key @"salitoskey ") ( Just "didem") nil
+-- >>>:t result
+-- >>>:t insert (Key @"semoskey") (Just "demir") result
+-- >>>:t insert (Key @"semsoskey") (Just True) result
+-- result :: OpenProduct Maybe '[ '("salitoskey ", String)]
+-- insert (Key @"semoskey") (Just "demir") result
+--   :: OpenProduct
+--        Maybe '[ '("semoskey", String), '("salitoskey ", String)]
+-- insert (Key @"semsoskey") (Just True) result
+--   :: OpenProduct
+--        Maybe '[ '("semsoskey", Bool), '("salitoskey ", String)]
+
+insert :: Key key -> f t -> OpenProduct f ts -> OpenProduct f ('(key, t) ': ts)
+insert _ ft (OpenProduct v) =  OpenProduct $ V.cons (Anyc ft) v
