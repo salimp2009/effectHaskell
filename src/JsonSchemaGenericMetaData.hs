6c69080ff5dd3bff1fe04dd23b4c51b64a1ae1c8
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-08-27 19:29:44 +0300

    continued to work on JSON Schema; added helper functions, corrected one function that did not work, added extra packages for pretty printing also started to modify IndexedVector from online videos

--- src/JsonSchemaGenericMetaData.hs
+++ src/JsonSchemaGenericMetaData.hs
@@ -11,11 +11,15 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
+--{-# LANGUAGE PolyKinds #-}
+--{-# LANGUAGE BangPatterns #-}
 
 module JsonSchemaGenericMetaData where
 
 import Control.Monad.Writer
-import Data.Aeson (Value (..), (.=), object)
+import Data.Aeson (Value (..), (.=), object, Key)
+import Data.Aeson.Key (fromText)
+import qualified Data.Aeson.Encode.Pretty as AEP ( encodePretty )
 import Data.Kind (Type, Constraint)
 import Data.Text (Text, pack)
 import Data.Typeable
@@ -23,7 +27,8 @@ import Data.Vector (fromList)
 import GHC.Generics
 import GHC.TypeLits
 import qualified GHC.TypeLits as Err
-import Data.Vector.Primitive (Vector(Vector))
+import qualified Data.ByteString as BString
+import qualified Data.ByteString.Lazy.Char8 as LC8 (putStrLn)
 
 -- | target is to create a JSON Schema like;
 {-
@@ -51,6 +56,24 @@ type GSchema :: (Type -> Type ) -> Constraint
 class GSchema a where
   gschema :: Writer [Text] Value
 
+instance  (KnownSymbol nm, KnownSymbol (ToJSONType a) )
+          => GSchema (M1 S ('MetaSel ('Just nm) _1 _2 _3)
+                           (K1 _4 a)) where
+  gschema = do
+      emitRequired @nm
+      pure . makePropertObj @nm $ makeTypeObject @a
+  {-# INLINE gschema #-} 
+  
+-- >>>testpp  
+-- Object (fromList [("myproperty",Object (fromList [("type",String "boolean")]))])
+testpp :: Value
+testpp = makePropertObj @"myproperty" $ makeTypeObject @Bool
+
+-- | pretty print for testing
+prettyJSON :: Value -> IO ()
+prettyJSON = LC8.putStrLn . AEP.encodePretty @Value 
+          --(makePropertObj @"myproperty" $ makeTypeObject @Bool)
+
 -- | Value is a JSON value as a Haskell value from aeson package
 {-
   data Value = Object !Object
@@ -60,14 +83,14 @@ class GSchema a where
            | Bool !Bool
            | Null
              deriving (Eq, Read, Typeable, Data, Generic)
--}  
+-}
 mergeObjects :: Value -> Value -> Value
 mergeObjects (Object a) (Object b) =   Object $ a <> b
 mergeObjects _ _ = error "unsafe use of mergeObjects"
 
 -- | use case;
 -- >>>emitRequired @"required property"
--- WriterT (Identity ((),["required property"]))
+-- NOW WriterT (Identity ((),["required property"]))
 emitRequired :: forall nm. KnownSymbol nm => Writer [Text] ()
 emitRequired = tell . pure . pack . symbolVal $ Proxy @nm
 -- emitRequired = do
@@ -130,9 +153,9 @@ type family TypeName (x :: Type) :: Symbol where
 {- " Retrieve the value associated with the given key of an 'Object'.
 -- The result is 'empty' if the key is not present or the value cannot
 -- be converted to the desired type."
-type signature : 
-(.:) :: (FromJSON a) => Object -> Key -> Parser a
--} 
+type signature : (kv stands for type class KeyValue -> Pair = (Key, Value))
+(.=) :: ToJSON v => Key -> v -> kv
+-}
 -- aeson package. also String is Text under the hood from the Value constructors
 -- >>> makeTypeObject @Int
 -- Object (fromList [("type",String "integer")])
@@ -142,9 +165,45 @@ makeTypeObject = object ["type" .= String (pack . symbolVal $ Proxy @(ToJSONType
 -- | also need to define object with the name of a property
 -- to be used the "properties"
 -- NOTE: this does not work as described in the book. Author also left as undefined
-makePropertObj :: forall name . KnownSymbol (ToJSONType name) => Value -> Value
-makePropertObj v = undefined
-  -- object [pack (symbolVal $ Proxy @name) .= v ]
+-- added Key to the import list and used fromText 
+-- >>>makePropertObj @"age" (makeTypeObject @String)
+-- WAS WAS Object (fromList [("age",Object (fromList [("type",String "integer")]))])
+-- WAS NOW Object (fromList [("age",Object (fromList [("type",String "string")]))])
+-- NOW Object (fromList [("age",Object (fromList [("type",String "string")]))])
+makePropertObj :: forall name . KnownSymbol name => Value -> Value
+makePropertObj v = object [fromText (pack (symbolVal $ Proxy @name)) .= v ]
 
-
-
+-- >>>:kind! Rep PersonN
+-- Rep PersonN :: * -> *
+-- = D1
+--     ('MetaData "PersonN" "JsonSchemaGenericMetaData" "main" 'False)
+--     (C1
+--        ('MetaCons "PersonN" 'PrefixI 'True)
+--        ((S1
+--            ('MetaSel
+--               ('Just "name")
+--               'NoSourceUnpackedness
+--               'NoSourceStrictness
+--               'DecidedLazy)
+--            (Rec0 String)
+--          :*: S1
+--                ('MetaSel
+--                   ('Just "age")
+--                   'NoSourceUnpackedness
+--                   'NoSourceStrictness
+--                   'DecidedLazy)
+--                (Rec0 Int))
+--         :*: (S1
+--                ('MetaSel
+--                   ('Just "phone")
+--                   'NoSourceUnpackedness
+--                   'NoSourceStrictness
+--                   'DecidedLazy)
+--                (Rec0 (Maybe String))
+--              :*: S1
+--                    ('MetaSel
+--                       ('Just "permissions")
+--                       'NoSourceUnpackedness
+--                       'NoSourceStrictness
+--                       'DecidedLazy)
+--                    (Rec0 [Bool]))))
---
+++ src/LengthIndexedVectors.hs
@@ -0,0 +1,68 @@
+--{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+module LengthIndexedVectors where
+import Data.Kind (Type)
+-- import Prelude hiding (and)
+
+-- | this is not the Nat we use in TypeLits
+-- used only for the example and not a very efficient one
+data Nat = Zero | Succ Nat
+  deriving (Show)
+
+type Vec n a = VectorID n a
+
+-- >>>:t (3:> 4:> 5:> VNil)
+-- (3:> 4:> 5:> VNil) :: Num a => VectorID ('Succ ('Succ ('Succ 'Zero))) a
+type VectorID :: Nat -> Type -> Type  
+data VectorID n a where
+  VNil  :: VectorID 'Zero a
+  -- VCons :: a -> VectorID n a -> VectorID ('Succ n) a   -- Original implementation
+  (:>) ::  a -> VectorID n a -> VectorID ('Succ n) a      -- << added later as a fancy constructor :)
+infixr 5 :>
+
+deriving stock instance Show a => Show (VectorID n a)
+
+-- | Original Show instance; above is a fancier way
+-- instance Show a => Show (VectorID n a) where
+--   show VNil = "VNil"
+--   show (VCons a as) = "VCons " <> show a <> " (" <> show as <> ")"
+
+-- >>>append  (1 :> 3 :> VNil) (2 :> VNil)  
+-- 1 :> (3 :> (2 :> VNil))
+append :: Vec n a -> Vec m a -> Vec (Add n m) a  
+append VNil v2 = v2
+-- append (VCons a rest) v2 = VCons a (append rest v2)  -- << Original implementation
+append (a :> rest ) v2 = a :> append rest v2
+
+-- >>>addNat (Succ(Succ Zero)) (Succ Zero)
+-- Succ (Succ (Succ Zero))
+addNat :: Nat -> Nat -> Nat
+addNat Zero m = m
+addNat (Succ nat) m = addNat nat (Succ m)
+
+-- >>>:kind! Add ('Succ('Succ 'Zero)) ('Succ 'Zero)
+-- Add ('Succ('Succ 'Zero)) ('Succ 'Zero) :: Nat
+-- = 'Succ ('Succ ('Succ 'Zero))
+type family Add (x::Nat) (y::Nat) where
+  Add 'Zero n = n
+  Add ('Succ n) m = 'Succ (Add n m)
+-- >>> andVec (True :> False :> VNil)  
+-- False
+
+-- >>> andVec (True :> True :> VNil) 
+-- True
+andVec :: Vec n Bool -> Bool
+andVec VNil = True
+andVec (b :> bs) = b && andVec bs
+
+-- >>>headVec $ append (1 :> 2 :> VNil) (3 :> 4 :> 5 :> VNil) 
+-- 1
+headVec :: Vec ('Succ n) a -> a
+headVec (x :> _) = x
+ 
