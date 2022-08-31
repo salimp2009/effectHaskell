ce3afca855c1a3dbea67bf90178715e09e9b5d83
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-08-31 22:14:09 +0300

    added KanExtensionsion; using Yoneda to avoid functor instance directly so that GHC can inline more efficiently in cases where the code is too polymorphic; "..code needs to do something in f,     it should use liftYoneda, and the final interface to your     generic code should make a call to lowerYoneda to hide it     as an implementation detail"

--- src/KanExtensionsExamples.hs
+++ src/KanExtensionsExamples.hs
@@ -20,3 +20,53 @@ import Control.Monad.Codensity
   - forall f. Monad f => f a, instead use
     forall f. Codensity f a
 -}
+
+{- 
+  Performance tips from book "Thinking with Types"
+  "
+    Whenever your generic code needs to do something in f,
+    it should use liftYoneda, and the final interface to your
+    generic code should make a call to lowerYoneda to hide it
+    as an implementation detail.
+    This argument holds exactly when replacing Functor
+    with Applicative or Monad, and Yoneda with Curried or
+    Codensity respectively.
+  " 
+-}
+
+-- >>>:t runYoneda
+-- runYoneda :: Yoneda f a -> forall b. (a -> b) -> f b
+
+-- >>>:t flip fmap
+-- flip fmap :: Functor f => f a -> (a -> b) -> f b
+
+-- >>>:kind! Yoneda [] Int 
+-- Yoneda [] Int :: *
+-- = Yoneda [] Int
+
+-- >>>:t liftYoneda
+-- liftYoneda :: Functor f => f a -> Yoneda f a
+-- >>>:t liftYoneda [] 
+-- liftYoneda [] :: Yoneda [] a
+
+-- >>>:t lowerYoneda (liftYoneda [])
+-- lowerYoneda (liftYoneda []) :: [a]
+
+-- >>>:t runCodensity
+-- runCodensity :: Codensity m a -> forall b. (a -> m b) -> m b
+
+
+-- >>>:t (>>=)
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+
+-- >>>:t runCurried @(Yoneda _) @(Yoneda _)
+-- runCurried @(Yoneda _) @(Yoneda _)
+--   :: Curried (Yoneda w1) (Yoneda w2) a
+--      -> forall r. Yoneda w1 (a -> r) -> Yoneda w2 r
+
+-- >>>:t flip (<*>)
+-- flip (<*>) :: Applicative f => f a -> f (a -> b) -> f b
+
+-- >>>:k Curried
+-- Curried :: (* -> *) -> (* -> *) -> * -> *
+
