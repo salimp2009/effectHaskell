7495ac1e01d21e74cce6eb3a75063c426e73f36c
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-09-04 14:10:27 +0300

    added some helper function for AdHOC singleton to go from to SBool and from SBool to Bool also started adding an example to show the usefullness of singleton started a logger using WriterT

--- src/AdHOCImplemntSingleton.hs
+++ src/AdHOCImplemntSingleton.hs
@@ -20,19 +20,57 @@ data SBool (b::Bool) where
   STrue  :: SBool 'True
   SFalse :: SBool 'False
 
--- >>>fromBool STrue
+-- >>>fromSBool STrue
 -- True
 
--- >>>fromBool SFalse
+-- >>>fromSBool SFalse
 -- False
-fromBool :: SBool b -> Bool
-fromBool STrue  = True
-fromBool SFalse = False
+fromSBool :: SBool b -> Bool
+fromSBool STrue  = True
+fromSBool SFalse = False
 
 data SomeSBool where
   SomeSBool :: SBool b -> SomeSBool
 
+-- >>>withSomeBool (SomeSBool STrue) fromSBool  
+-- True
+
+-- >>>withSomeBool (toSBool False) fromSBool
+-- False
 withSomeBool :: SomeSBool -> (forall (b::Bool). SBool b -> r) -> r
 withSomeBool (SomeSBool sb)  f = f sb
 
+toSBool :: Bool -> SomeSBool
+toSBool True = SomeSBool STrue
+toSBool False= SomeSBool SFalse
+
+-- | example of usefull of singletons; ??
+-- Build monad stack to conditionally log messages
+-- messages will be for debugging purposes only
+-- we can conditionally choose our monad stack depending on a runtime value; ??
+
+-- | Beginning with a typeclass indexed over Bool
+-- it has an assocaiate type family to select the correct Monad stack
+-- plus some methods to work with the stack
+class Monad (LoggingMonad b) => MonadLogging (b::Bool) where
+  type LoggingMonad b = (r :: Type -> Type) | r -> b         
+  logMsg :: String -> LoggingMonad b ()
+  runLogging :: LoggingMonad b a -> IO a
+
+  -- ^ | r->b is known as type family dependency
+  -- ^ it means if we know LoggingMonad b we can infer b
+  
+  -- | 'False instance will be used to ignore any attempts to log messages
+instance MonadLogging 'False where
+  type LoggingMonad 'False = IO
+  logMsg _ = pure () 
+  runLogging = id 
+
+instance MonadLogging 'True where
+  type LoggingMonad 'True = WriterT [String] IO
+  logMsg s = tell [s]
+  runLogging m = do
+    (a, w) <- runWriterT m
+    for_ w putStrLn
+    pure a
 
