b0b841228e2c47e0a6cc75d51010e708b4c6e4fa
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-09-12 15:13:23 +0300

    finished Dependent Types and started basic Template Haskell examples

---
+++ src/TemplateHaskellBasic.hs
@@ -0,0 +1,72 @@
+--{-# LANGUAGE ConstraintKinds #-}
+--{-# LANGUAGE EmptyCase #-}
+--{-# LANGUAGE FlexibleInstances #-}
+--{-# LANGUAGE GADTs #-}
+--{-# LANGUAGE InstanceSigs #-}
+--{-# LANGUAGE MultiParamTypeClasses #-}
+--{-# LANGUAGE RankNTypes #-}
+--{-# LANGUAGE ScopedTypeVariables #-}
+--{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+--{-# LANGUAGE TypeFamilies #-}
+--{-# LANGUAGE TypeInType #-}
+--{-# LANGUAGE UndecidableInstances #-}
+--{-# LANGUAGE ScopedTypeVariables #-}
+--{-# LANGUAGE DataKinds           #-}
+-- {-# LANGUAGE StandaloneKindSignatures #-}
+-- {-# LANGUAGE DerivingStrategies #-}
+
+module TemplateHaskellBasic where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+-- >>>$expTH
+-- 3
+
+-- >>>:t expTH
+-- expTH :: Q Exp
+
+-- >>>$expTH
+-- 3
+
+-- >>>runQ expTH
+-- InfixE (Just (LitE (IntegerL 1))) (VarE GHC.Num.+) (Just (LitE (IntegerL 2)))
+
+-- >>>:t runQ
+-- runQ :: Quasi m => Q a -> m a
+expTH :: Q Exp
+expTH = [| 1 + 2 |]
+
+-- >>>:t answer
+-- answer :: Exp
+
+-- >>>$(pure answer)
+-- 32
+
+-- >>>$$(liftTyped (5::Int))
+-- 5
+
+-- >>>:t ( $$(liftTyped (5::Int)) )
+-- ( $$(liftTyped (5::Int)) ) :: Int
+
+-- >>>$$([|| 1 + 2 ||])
+-- 3
+
+-- >>>:t ([|| 1 + 2 ||])
+-- ([|| 1 + 2 ||]) :: (Quote m, Num a) => Code m a
+answer :: Exp
+answer = LitE (IntegerL 32)
+
+-- >>>:t $hello
+-- $hello :: IO ()
+
+-- >>>runQ hello
+-- AppE (VarE System.IO.putStrLn) (LitE (StringL "Hello Template Haskell"))
+
+-- ->$hello
+-- ->Hello Template Haskell
+hello :: Q Exp
+hello = [|putStrLn "Hello Template Haskell"|]
