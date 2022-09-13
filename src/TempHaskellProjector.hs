1dbb14f3fb1bf1b977e04f53967d369a54e3db2a
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-09-13 15:29:09 +0300

    added more TH expression of the same tuple access for tuples with 3 or more elems

--- src/TempHaskellProjector.hs
+++ src/TempHaskellProjector.hs
@@ -41,14 +41,12 @@ import Language.Haskell.TH.Syntax
 -- >>>:t [| \(x, _, _) -> x |]
 -- [| \(x, _, _) -> x |] :: Quote m => m Exp
 proj :: Int -> Int -> Q Exp
-proj n k = do
+proj n k = do 
   x <- newName "x"
   let makePat j
         | j == k = VarP x
         | otherwise = WildP
   pure $ LamE [TupP $ map makePat [0..n-1]] (VarE x)
-
-
 
 
 --- >>>$(proj 3 2) ("aaa", "bbb", "ccc")  
@@ -60,4 +58,62 @@ proj n k = do
 -- >>>:t proj
 -- proj :: Int -> Int -> Q Exp
 
+-- >>>$(proj 3 4) ("aaa", "bbb", "ccc") 
+-- The exact Name ‘x_aItl’ is not in scope
+--   Probable cause: you used a unique Template Haskell name (NameU), 
+--   perhaps via newName, but did not bind it
+--   If that's it, then -ddump-splices might be useful
+
+-- | need to revise the code to check requested index is not 
+-- greater than the total num of elems
+
+-- >>>$(proj2 3 2) ("aaa", "bbb", "ccc")
+-- "ccc"
+
+-- >>>$(proj2 3 3) ("aaa", "bbb", "ccc")
+-- Incorrect projection3 of 3 elements 
+proj2 :: Int -> Int -> Q Exp
+proj2 n k  
+  | n > 1 && 0 <=k && k < n = do
+          x <- newName "x"
+          let makePat j
+                | j == k = VarP x
+                | otherwise = WildP
+          pure $ LamE [TupP $ map makePat [0..n-1]] (VarE x)
+  | n <=1 = fail "Number of tuples elements must be > 1" 
+  | otherwise = fail $ "Incorrect projection" 
+                <> show k <> " of " <> show n <>" elements "
+
+
+-- >>>$(proj3 3 2) ("aaa", "bbb", "ccc")
+-- "ccc"
+
+-- >>>$(proj3 3 3) ("aaa", "bbb", "ccc")
+-- Incorrect projection3 of 3 elements 
+
+-- | need to refactor code to delegate parts of it
+-- to GHC with quotes and splices
+-- TH allows nesting splices and quotes
+-- using varE, varP, wildP, tupP functions instead of VarE, VarP, WildP, TupP.
+-- these functions returns a Q monad 
+-- they work better with the $(…) splice operator which
+-- expects Q a values inside it
+proj3 :: Int -> Int -> Q Exp
+proj3 n k  
+  | n > 1 && 0 <=k && k < n = do
+          x <- newName "x"
+          [| \ $(mkArg x) -> $(varE x)|]
+  | n <=1 = fail "Number of tuples elements must be > 1" 
+  | otherwise = fail $ "Incorrect projection" 
+                <> show k <> " of " <> show n <>" elements "
+  where
+    mkPat x j 
+          | j == k = varP x
+          | otherwise = wildP
+    mkArg x = tupP $ map (mkPat x ) [0..n-1]  
+    
+-- >>>:t TupP    
+-- TupP :: [Pat] -> Pat
+-- >>>:t tupP
+-- tupP :: Quote m => [m Pat] -> m Pat
 
