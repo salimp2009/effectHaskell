2ab8431b27db1fc88558c8e7dc3270cb03488745
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-09-25 17:20:28 +0300

    added helper functions to precompute a lookup Table that has precompute values and a function to compute for a value that is not the precomputed Table

--- src/TemplateHaskLookupTable.hs
+++ src/TemplateHaskLookupTable.hs
@@ -22,9 +22,34 @@ module TemplateHaskLookupTable where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
+import GHC.Float (int2Double)
 
+{- 
+  The goal is as described in "Production Haskell" book;
+  to pass in a list of common numbers, 
+  run the function on each to precompute them, and
+  if we didn’t cache the number, have
+  a clause that passes the value to the math function 
+-}
 
--- precompute :: [Int] -> DecsQ
--- precompute xs = do 
---       -- ..............
---       return [FunD Name [Clause] ]
+precompute :: [Int] -> DecsQ
+precompute xs = do 
+      let name = mkName "lookupTable"
+          patterns = map intToPat xs
+          fnBodies = map precomputeInteger xs
+          clauses =  precompClauses fnBodies patterns
+
+      return [FunD name clauses ]
+  where
+    intToPat :: Int -> Pat
+    intToPat = LitP . IntegerL . toInteger
+
+    precomputeInteger :: Int -> Exp
+    precomputeInteger = LitE . DoublePrimL . toRational . bigMathProblem
+
+    precompClauses :: [Exp] -> [Pat] -> [Clause]
+    precompClauses = zipWith (\body patrn -> Clause [patrn] (NormalB body) [])
+      
+    
+bigMathProblem :: Int -> Double  
+bigMathProblem x = int2Double (x+100) * 10.12345
