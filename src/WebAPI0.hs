78e5f6075563aad5b76e84abf5869184609b089d
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-05 16:32:38 +0300

    added types related to WebAPI specifically for database which has books rating title year and also a server status

--- src/WebAPI0.hs
+++ src/WebAPI0.hs
@@ -1,16 +1,34 @@
 module WebAPI0 where
 
+-- | getReq is the main object in this WebApi example
+-- that mimic a client request and a Haskell web service
+-- that gets and check the request from a data base and
+-- send repsonse to client
+-- this module represents the getting the request and checking 
+
+-- >>> getReq ["title", "324534"]
+-- "Haskel in Depth"
+
+-- >>> getReq ["year", "324534"]
+-- "2021"
 getReq :: [String] -> IO String
-getReq []      = pure "Ok"
+getReq []      = pure "OK"
 getReq [op, _] = 
     case op of 
       "title"   -> pure "Haskel in Depth"
       "year"    -> pure "2021"
       "rating"  -> pure "Great"
       _         -> ioError (userError "not implemented")
-getReq _       = ioError (userError "Malformed Request")     
+getReq _       = ioError (userError "Malformed Request") 
 
-
+checkReq :: IO()
+checkReq = do
+  b <- getReq []
+  y <- getReq ["year", "7548"]
+  putStrLn (if b=="OK" && y == "2021"
+            then "OK"
+            else "Wrong Answer"
+           )
 
 
 
--- src/WebAPI1.hs
+++ src/WebAPI1.hs
@@ -1,1 +1,41 @@
 module WebAPI1 where
+import Data.Kind (Type)
+
+-- | Book rating and services can be represented enumarating the 
+-- potential values
+data Rating = Bad | Good | Great
+  deriving Show
+
+data ServiceStatus = Ok | Down
+  deriving Show
+
+-- | Request handlers will be represented will specific
+-- type synonym rather than plain String
+type BookID          = Int
+type HandlerAction a = IO a
+type ReqHandler a    = BookID -> HandlerAction a
+
+data BookInfoAPIImpl = BookInfoAPIImpl 
+                        { root   :: HandlerAction ServiceStatus
+                        , title  :: ReqHandler String
+                        , year   :: ReqHandler Int
+                        , rating :: ReqHandler Rating
+                        }
+
+impl1 :: BookInfoAPIImpl
+impl1 = BookInfoAPIImpl
+         { root   = pure Ok
+         , title  = \_ -> pure "Haskell In Depth" 
+         , year   = \_ -> pure 2021
+         , rating = \_ -> pure Great
+         }       
+         
+impl2 :: BookInfoAPIImpl
+impl2 = BookInfoAPIImpl
+         { root   = pure Down
+         , title  = const notImplemented 
+         , year   = const notImplemented
+         , rating = const notImplemented
+         }            
+    where 
+      notImplemented = ioError (userError "not implemented")
