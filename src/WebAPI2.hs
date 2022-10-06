523ff6a298fc8e06679804d90c2d6a4fbe64a6bb
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-06 16:10:20 +0300

    started implementing the request and server API and request implementations as type level functions and will use type families to get responses

--- src/WebAPI2.hs
+++ src/WebAPI2.hs
@@ -1,1 +1,38 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 module WebAPI2 where
+
+import Data.Kind
+import GHC.TypeLits
+
+import WebAPI1
+
+-- | describes results ; what we are getting 
+-- as a response
+data Get (a::Type)
+
+-- |captures a request parameter
+data Capture (a::Type)
+
+-- | lists the alternative request operations
+data a :<|> b = a:<|> b
+infixr 8 :<|>
+
+-- |
+data (a :: k) :> (b :: Type)
+infixr 9 :> 
+
+type BookInfoAPI =
+      Get ServiceStatus
+    :<|> "title" :> Capture BookID :> Get String
+    :<|> "year"  :> Capture BookID :> Get Int
+    :<|> "rating"  :> Capture BookID :> Get Rating
+
+type BookInfoAPIImpl2 =
+        HandlerAction ServiceStatus
+  :<|> (BookID -> HandlerAction String)     
+  :<|> (BookID -> HandlerAction Int)     
+  :<|> (BookID -> HandlerAction Rating)     
