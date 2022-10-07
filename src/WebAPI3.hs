a5170dadad3bd589fe0b483e7c93ca24366b13e3
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-07 21:44:06 +0300

    added more instance of HasServer to cover all type family instance of Server; only one left and an update get implementation to get the Results

--- src/WebAPI3.hs
+++ src/WebAPI3.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
 module WebAPI3 where
 
 import Control.Applicative ((<|>))
 import WebAPI2
 import WebAPI1
-import Data.Proxy (Proxy)
+import Data.Proxy (Proxy(..), Proxy)
+import GHC.TypeLits
 
 {- 
   "The FlexibleInstances GHC extension is almost always needed 
@@ -28,5 +33,28 @@ instance Show a => HasServer (Get a) where
   routeS _ handler [] = Just (encode $ handler )
   routeS _ _       _  = Nothing
 
+instance {-# OVERLAPS #-} HasServer (Get String) where  
+  routeS :: Proxy (Get String) 
+         -> IO String
+         -> Request
+         -> Maybe (IO String)
+  routeS _ handler [] = Just handler 
+  routeS _ _       _  = Nothing        
+
+
+instance (HasServer a, HasServer b) => HasServer (a :<|> b) where 
+  routeS :: Proxy (a :<|> b) 
+         -> (Server a :<|> Server b) 
+         -> Request
+         -> Maybe (IO String)
+  routeS _ (handlera :<|> handlerb) xs = 
+         routeS (Proxy :: Proxy a) handlera xs
+     <|> routeS (Proxy :: Proxy b) handlerb xs
+
+instance  (KnownSymbol s, HasServer r) => HasServer ((s::Symbol) :> r)   where
+  routeS :: Proxy (s :> r) -> Server r -> Request -> Maybe (IO String)
+  routeS _ handler (x : xs)
+      | symbolVal (Proxy :: Proxy s) == x = routeS (Proxy :: Proxy r) handler xs
+  routeS _ _      _ =  Nothing  
 
 
