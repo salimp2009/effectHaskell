90b1b8a6d0daa522ff8a98febb0cddbfb42c50ec
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-13 23:34:51 +0300

    added a manual implementation of Singleton and Dependent types for elevator which opens door if the elevator has door Closed; by using type

--- src/Elevators/SingManual.hs
+++ src/Elevators/SingManual.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 module Elevators.SingManual where
+import Data.Coerce (coerce)
 
 data DoorStateN = OpenedN | ClosedN
   deriving (Eq, Show)
@@ -35,11 +36,29 @@ doorState MkDoor =
 instance Show (Door s) where
   show d = "Door " <> show (doorState d)
 
--- >>> doorState (MkDoor @ClosedN ) 
+-- >>> doorState (MkDoor @('ClosedN)) 
+-- ClosedN
+
+-- >>>doorState (MkDoor :: Door ClosedN)
+-- ClosedN
+
+-- >>>doorState (MkDoor :: Door 'ClosedN)
 -- ClosedN
 
 
+openN :: Door 'ClosedN -> Door 'OpenedN
+openN _ = MkDoor
 
+closeN :: Door 'OpenedN -> Door 'ClosedN
+closeN _ = MkDoor
 
-  
+data SomeDoor where
+  SomeDoor :: Door s -> SomeDoor
+
+deriving instance Show SomeDoor  
+
+parseDoor :: String -> Maybe SomeDoor
+parseDoor "Opened" = Just $ SomeDoor (MkDoor @('OpenedN))
+parseDoor "Closed" = Just $ SomeDoor (MkDoor :: Door ClosedN)
+parseDoor _        = Nothing  
 
