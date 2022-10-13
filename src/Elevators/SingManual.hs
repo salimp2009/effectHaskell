0420760a6f2051e341bb6178c30c51a8034c4f79
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-13 16:40:01 +0300

    started adding Singleton to implement Dependent types and create a safer Elevator interface moving values to type level to check the state of the doors so that it will only be possible to call certain functions depending on their types

--- src/Elevators/SingManual.hs
+++ src/Elevators/SingManual.hs
@@ -4,10 +4,42 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 module Elevators.SingManual where
 
-data DoorStateN = Opened | Closed
+data DoorStateN = OpenedN | ClosedN
   deriving (Eq, Show)
+
+data SDoorState (s::DoorStateN) where
+  SClosed :: SDoorState 'ClosedN
+  SOpened :: SDoorState 'OpenedN
+
+class SDoorStateI (s::DoorStateN)  where
+  sDoorState :: SDoorState s
+
+instance SDoorStateI OpenedN where
+  sDoorState = SOpened
+  
+instance SDoorStateI ClosedN where
+    sDoorState = SClosed 
+    
+data Door (s::DoorStateN) where
+  MkDoor :: SDoorStateI s => Door s
+
+doorState :: forall s. Door s -> DoorStateN  
+doorState MkDoor =
+    case sDoorState :: SDoorState s of
+        SClosed -> ClosedN
+        SOpened -> OpenedN
+
+instance Show (Door s) where
+  show d = "Door " <> show (doorState d)
+
+-- >>> doorState (MkDoor @ClosedN ) 
+-- ClosedN
+
+
+
 
   
 
