a616b7713a53b652043cd4f921e0df296f8c0ab1
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-17 16:19:50 +0300

    added prev function but too much abstracted LEProof types makes it harder to understand so will not continue this example further

--- src/Elevators/Floors.hs
+++ src/Elevators/Floors.hs
@@ -51,6 +51,64 @@ type BelowTop mx cur = LE (S cur) mx
 next :: forall (mx::Nat) (cur::Nat). BelowTop mx cur => FloorK mx cur -> FloorK mx (S cur)
 next MkFloorK = MkFloorK -- @mx @(S cur)
 
+prev :: forall (mx::Nat) (cur::Nat). FloorK mx ('S cur) -> FloorK mx cur
+prev MkFloorK = 
+          withSNat snatCur $ 
+            withLEProof leCur
+              MkFloorK
+          where
+            snatCur :: SNat cur
+            snatCur = case snat :: SNat ('S cur ) of
+              SS -> snat
+            
+            leCur :: LEProof cur mx
+            leCur = leStepL leProof
+
+-- >>>:i leStepL            
+-- leStepL :: LEProof ('S n) m -> LEProof n m
+
+-- >>>:i SNat
+-- type role SNat nominal
+-- type SNat :: Nat -> *
+-- data SNat n where
+--   SZ :: SNat 'Z
+--   SS :: SNatI n1 => SNat ('S n1)
+--   	-- Defined in ‘Data.Type.Nat’
+-- instance Show (SNat p) -- Defined in ‘Data.Type.Nat’
+-- instance TestEquality SNat -- Defined in ‘Data.Type.Nat’
+
+-- >>>:i SNatI
+-- type SNatI :: Nat -> Constraint
+-- class SNatI n where
+--   induction :: f 'Z
+--                -> (forall (m :: Nat). SNatI m => f m -> f ('S m)) -> f n
+--   {-# MINIMAL induction #-}
+--   	-- Defined in ‘Data.Type.Nat’
+-- instance SNatI 'Z -- Defined in ‘Data.Type.Nat’
+-- instance SNatI n => SNatI ('S n) -- Defined in ‘Data.Type.Nat’
+
+-- >>>:k LE Nat0 Nat2
+-- LE Nat0 Nat2 :: Constraint
+
+-- >>>:t withSNat
+-- withSNat :: SNat n -> (SNatI n => r) -> r
+
+-- >>>:t withLEProof
+-- withLEProof :: LEProof n m -> (LE n m => r) -> r
+
+-- >>>:i LEProof
+-- type role LEProof nominal nominal
+-- type LEProof :: Nat -> Nat -> *
+-- data LEProof n m where
+--   LEZero :: LEProof 'Z m
+--   LESucc :: LEProof n1 m1 -> LEProof ('S n1) ('S m1)
+--   	-- Defined in ‘Data.Type.Nat.LE’
+-- instance [safe] Show (LEProof n m) -- Defined in ‘Data.Type.Nat.LE’
+-- instance [safe] Eq (LEProof n m) -- Defined in ‘Data.Type.Nat.LE’
+-- instance [safe] Ord (LEProof n m) -- Defined in ‘Data.Type.Nat.LE’
+-- instance [safe] (SNatI n, SNatI m) => Decidable (LEProof n m)
+--   -- Defined in ‘Data.Type.Nat.LE’
+
 -- >>>:t show (MkFloorK  @Nat5 @Nat2)
 -- show (MkFloorK  @Nat5 @Nat2) :: String
 
