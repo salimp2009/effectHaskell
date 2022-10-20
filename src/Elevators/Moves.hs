3553bd95a0b69133278a929973bc58a7a64f23ae
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-20 14:56:08 +0300

    started Move actions; deciding on to go up, down or standstill; need to implement decideMove function and a couple proofs & pattern matching

--- src/Elevators/Moves.hs
+++ src/Elevators/Moves.hs
@@ -1,1 +1,50 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
 module Elevators.Moves where
+
+import Data.Type.Nat
+import Data.Type.Nat.LE
+import Data.Type.Dec
+import Data.Type.Equality
+import Data.Void
+
+import Elevators.Floors 
+
+data Move mx to from where
+  StandStill :: Move mx to to
+  GoingUp    :: BelowTop mx from => Move mx to from
+  GoingDown  :: from ~ S fl => Move mx to from 
+
+
+decideMove :: forall mx to from
+            . FloorK mx to -> FloorK mx from -> Move mx to from  
+decideMove = undefined
+
+
+-- >>>:i discreteNat
+-- discreteNat :: (SNatI n, SNatI m) => Dec (n :~: m)
+--   	-- Defined in ‘Data.Type.Nat’
+
+-- >>>:i Dec
+-- type Dec :: * -> *
+-- data Dec a = Yes a | No (Neg a)
+--   	-- Defined in ‘Data.Type.Dec’
+
+-- >>>:i decideLE
+-- decideLE :: (SNatI n, SNatI m) => Dec (LEProof n m)
+--   	-- Defined in ‘Data.Type.Nat.LE’
+
+-- >>>:i LEProof
+-- type role LEProof nominal nominal
+-- type LEProof :: Nat -> Nat -> *
+-- data LEProof n m where
+--   LEZero :: LEProof 'Z m
+--   LESucc :: LEProof n1 m1 -> LEProof ('S n1) ('S m1)
+--   	-- Defined in ‘Data.Type.Nat.LE’
+-- instance [safe] (SNatI n, SNatI m) => Decidable (LEProof n m)
+--   -- Defined in ‘Data.Type.Nat.LE’
