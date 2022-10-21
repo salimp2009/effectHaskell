dd25734d8de0a32c0056d6f4e23251c7e2ad74a5
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2022-10-21 22:49:21 +0300

    started implement a safe interface for elevators and a simulation; imcomplete

--- src/Elevators/SafeElev.hs
+++ src/Elevators/SafeElev.hs
@@ -1,1 +1,27 @@
-module Elevators.SafeElev where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Elevators.SafeElev 
+            ( module X
+
+            )
+            where
+
+import Control.Monad.Trans
+import Data.Type.Equality
+import Numeric.Natural
+import Data.Type.Nat
+import Data.Proxy
+
+import Elevators.Floors as X
+import Elevators.Operations as X
+import Elevators.Moves as X
+
+moveTo :: MonadIO m 
+       => FloorK mx to -> ElevatorK mx from 'ClosedK -> m (ElevatorK mx to 'ClosedK)
+moveTo flr el = -- undefined
+  case decideMove flr (currentFloor el) of 
+    StandStill -> pure el
+    GoingUp    -> up el >>= moveTo flr
+    GoingDown  -> down el >>= moveTo flr    
